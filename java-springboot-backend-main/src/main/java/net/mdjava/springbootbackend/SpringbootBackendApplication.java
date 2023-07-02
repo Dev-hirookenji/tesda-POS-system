32b3c22d9707c35dabcf8e6a1171aa56645f88e0
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-07-02 21:00:36 +0800

    working na

--- java-springboot-backend-main/src/main/java/net/mdjava/springbootbackend/SpringbootBackendApplication.java
+++ java-springboot-backend-main/src/main/java/net/mdjava/springbootbackend/SpringbootBackendApplication.java
@@ -1,8 +1,8 @@
 package net.mdjava.springbootbackend;
 
-// import net.mdjava.springbootbackend.model.Category;
-// import net.mdjava.springbootbackend.repository.CategoryRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
+import net.mdjava.springbootbackend.model.Category;
+import net.mdjava.springbootbackend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,19 +14,19 @@ public class SpringbootBackendApplication implements CommandLineRunner {
         SpringApplication.run(SpringbootBackendApplication.class, args);
     }
     
-    // @Autowired
-    // private CategoryRepository categoryRepository;
+    @Autowired
+     private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        // Category category = new Category();
-        // category.setName("Food");
-        // category.setDescription("The description of Food");
-        // categoryRepository.save(category);
+        Category category = new Category();
+        category.setName("Food");
+        category.setDescription("The description of Food");
+        categoryRepository.save(category);
         
-        // Category category1 = new Category();
-        // category1.setName("Beverage");
-        // category1.setDescription("The description of Beverage");
-        // categoryRepository.save(category1);
+        Category category1 = new Category();
+        category1.setName("Beverage");
+        category1.setDescription("The description of Beverage");
+        categoryRepository.save(category1);
     }
 }
